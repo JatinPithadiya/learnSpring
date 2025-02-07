a9452e101d65eadc67af71504a3e867a361a0b35
Jatin Pithadiya <jatinpithadiya6@gmail.com>
2025-02-07 21:16:57 +0000

    added hello world api

---
+++ restful-web-services/src/main/java/com/learn/rest/webservices/restful_web_services/helloworld/HelloworldController.java
@@ -0,0 +1,19 @@
+package com.learn.rest.webservices.restful_web_services.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloworldController {
+
+    @GetMapping(path = "/hello-world")
+    public String helloworld(){
+        return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloworldBean(){
+        return new HelloWorldBean("Hello-World");
+    }
+
+}
