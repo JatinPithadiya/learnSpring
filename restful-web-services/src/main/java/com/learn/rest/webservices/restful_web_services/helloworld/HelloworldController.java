a3eb4d7082c70706c269d095ea856d4188362128
Jatin Pithadiya <jatinpithadiya6@gmail.com>
2025-02-14 17:22:03 +0000

    add user resource with api

--- restful-web-services/src/main/java/com/learn/rest/webservices/restful_web_services/helloworld/HelloworldController.java
+++ restful-web-services/src/main/java/com/learn/rest/webservices/restful_web_services/helloworld/HelloworldController.java
@@ -1,6 +1,7 @@
 package com.learn.rest.webservices.restful_web_services.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,6 +15,11 @@ public class HelloworldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloworldBean(){
         return new HelloWorldBean("Hello-World");
+    }
+
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloworldPathVariable(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello World %s", name));
     }
 
 }
---
+++ restful-web-services/src/main/java/com/learn/rest/webservices/restful_web_services/user/UserDaoService.java
@@ -0,0 +1,28 @@
+package com.learn.rest.webservices.restful_web_services.user;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDaoService {
+
+    public static List<User> users = new ArrayList<>();
+
+    static{
+        users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
+        users.add(new User(2, "Eve", LocalDate.now().minusYears(25)));
+        users.add(new User(3, "Jim", LocalDate.now().minusYears(20)));
+    }
+
+    public List<User> findAll(){
+        return users;
+    }
+
+    public User findUser(Integer id){
+        return users.stream().filter(user -> user.getId().equals(id)).findFirst().get();
+    }
+}
