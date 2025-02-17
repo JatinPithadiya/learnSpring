0129b6d042df8b1f07720114a325a60a48f1d5e7
Jatin Pithadiya <jatinpithadiya6@gmail.com>
2025-02-17 12:13:29 +0000

    update http status code and response with location in it

--- restful-web-services/src/main/java/com/learn/rest/webservices/restful_web_services/user/UserResource.java
+++ restful-web-services/src/main/java/com/learn/rest/webservices/restful_web_services/user/UserResource.java
@@ -1,5 +1,6 @@
 package com.learn.rest.webservices.restful_web_services.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponents;
 
 @RestController
 public class UserResource {
@@ -30,8 +33,13 @@ public class UserResource {
 
     @PostMapping("/users")
     public ResponseEntity<User> createUser(@RequestBody User user){
-         service.save(user);
+         User savedUser = service.save(user);
 
-         return ResponseEntity.created(null).build();
+        URI location  = ServletUriComponentsBuilder.fromCurrentRequest()
+            .path("/{id}")
+            .buildAndExpand(savedUser.getId())
+            .toUri();
+
+         return ResponseEntity.created(location).build();
     }
 }
